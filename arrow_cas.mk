b4374be6e345c059d6cecf2cd8df5900d394afc4
Xayah <zds1249475336@gmail.com>
2021-07-29 19:25:08 +0800

    Arrow ify

--- arrow_cas.mk
+++ arrow_cas.mk
@@ -11,15 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cas device
 $(call inherit-product, device/xiaomi/cas/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_cas
+PRODUCT_NAME := arrow_cas
 PRODUCT_DEVICE := cas
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2007J1SC
+
+DEVICE_MAINTAINER := Xayah
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
